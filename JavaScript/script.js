db0eaac00c1af5fdf542e5f98f0cc5a57b514105
Bhavya <jainbhavya154@gmail.com>
2023-12-21 00:50:29 +0530

    Javascript Added

---
+++ JavaScript/script.js
@@ -0,0 +1,17 @@
+alert("Hello World");
+        console.log("Code is running...")
+        console.log("Code Is WOW...")
+        var a = prompt("Enter your number")
+        var isTrue = confirm("Are you happy?")
+        if(isTrue){
+            console.log("Hello")
+        }
+        else{
+            console.log("Bye")
+            document.body.style.backgroundColor = "red"
+        }
+        console.log("Your number is  " + a)
+
+        document.title = "Hey I Am Good"
+
+        
